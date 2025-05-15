cec3915b4e9ac11c11c7122ee9f98f79a838aae1
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-15 22:16:46 +0500

    Add user table schema using drizzle ORM

---
+++ configs/schema.js
@@ -0,0 +1,12 @@
+import { varchar } from "drizzle-orm/mysql-core";
+import { pgTable, serial, varchar, boolean  } from 'drizzle-orm/pg-core';
+
+
+export const USER_TABLE= pgTable('users',
+    {
+        id: serial().primaryKey(),
+        name: varchar().notNull(),
+        email: varchar().notNull().unique(),
+        isMember: boolean().default(false),
+    }
+)
