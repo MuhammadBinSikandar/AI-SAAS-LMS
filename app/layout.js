d1ea97e6a58964b03ddf0b5250baea92acc909f5
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-15 22:16:24 +0500

    Refactor layout.js to replace Geist font with Outfit and wrap layout in ClerkProvider

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Outfit} from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit=Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -18,12 +14,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={outfit.className}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
