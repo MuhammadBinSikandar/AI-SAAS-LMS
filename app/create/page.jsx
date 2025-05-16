44a83a2e81e517a3a4db2269152aaf8a9445b270
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-16 15:26:44 +0500

    Add Create component for generating personal study material

---
+++ app/create/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+    return (
+        <div className='flex flex-col items-center p-5 md:px-24 lg:px-36'>
+            <h2 className='font-bold text-4xl text-blue-700'>Start Building Your Personal Study Material</h2>
+            <p className='text-gray-500 text-lg'>Fill all details to generate study material for you next project</p>
+        </div>
+    )
+}
+
+export default Create
