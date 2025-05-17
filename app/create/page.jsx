c1855189ff108d0b58b906119630330c0733657b
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-17 20:34:46 +0500

    Add Create component with initial state and SelectOption integration

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,10 +1,17 @@
+"use client"
 import React from 'react'
+import SelectOption from './_components/SelectOption'
+import { useState } from 'react'
 
 function Create() {
+    const [step, setStep] = useState(0);
     return (
-        <div className='flex flex-col items-center p-5 md:px-24 lg:px-36'>
+        <div className='flex flex-col items-center mt-15 p-5 md:px-24 lg:px-36'>
             <h2 className='font-bold text-4xl text-blue-700'>Start Building Your Personal Study Material</h2>
             <p className='text-gray-500 text-lg'>Fill all details to generate study material for you next project</p>
+            <div className='mt-10'>
+                {step == 0 ? <SelectOption /> : null}
+            </div>
         </div>
     )
 }
