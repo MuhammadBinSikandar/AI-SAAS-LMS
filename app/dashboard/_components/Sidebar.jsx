9918562c2d6906549aef6efbd5cdb6df8378d9d8
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-18 15:49:50 +0500

    feat: wrap "Create New" button in Link component for navigation

--- app/dashboard/_components/Sidebar.jsx
+++ app/dashboard/_components/Sidebar.jsx
@@ -35,7 +35,9 @@ function Sidebar() {
                 <h2 className='font-bold text-2xl'>SkillPilot</h2>
             </div>
             <div className='mt-10'>
-                <Button className="w-full">+ Create New</Button>
+                <Link className='w-full' href={'/create'}>
+                    <Button className="w-full">+ Create New</Button>
+                </Link>
                 <div className='mt-5'>
                     {MenuList.map((menu, index) => (
                         <div key={index} className={`flex gap-5 items-center p-3 hover:bg-slate-200 rounded-lg cursor-pointer mt-3 ${path == menu.path && 'bg-slate-200'}`}>
