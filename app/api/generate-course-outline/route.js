c70ac4d404d4cced1a4c9867ac37d6455c1994e2
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2025-05-21 06:32:16 +0500

    feat: enhance course outline generation by adding Inngest integration for event handling

--- app/api/generate-course-outline/route.js
+++ app/api/generate-course-outline/route.js
@@ -2,11 +2,12 @@ import { generateCourseOutline } from "@/configs/AiModel";
 import { NextResponse } from "next/server";
 import { db } from "@/configs/db";
 import { STUDY_MATERIAL_TABLE } from "@/configs/schema";
+import { inngest } from "@/inngest/client";
 
 export async function POST(req) {
     try {
         const { courseId, topic, studyType, difficultyLevel, createdBy } = await req.json();
-        const PROMPT = `Generate a comprehensive and detailed study material for ${topic} designed for ${studyType} at a ${difficultyLevel} difficulty level. The study material should be extensive, covering all essential aspects of the topic to ensure thorough learning. Include the following in JSON format: 1. Course Title (as course_title make sure to use a good name) 2. Diffculty Level (difficulty)   3. **Course Summary**: A detailed overview of the course (50-100 words with key as CourseSummary), including its objectives, target audience, and key learning outcomes.  4. **List of Chapters**: A minimum of 8 chapters, each with:- **Chapter Title**: A clear and descriptive title.- **Chapter Summary**: A detailed summary of the chapter’s content (at least 100 words per chapter).- **Topic List**: A list of at least 5 specific topics covered in the chapter, each with a brief description (2-3 sentences per topic). 5. **Additional Resources**: Suggest at least 3 resources (e.g., books, websites, or videos) relevant to the course, with a brief description of each.`
+        const PROMPT = `Generate a comprehensive and detailed study material for ${topic} designed for ${studyType} at a ${difficultyLevel} difficulty level. The study material should be extensive, covering all essential aspects of the topic to ensure thorough learning. Include the following in JSON format: 1. Course Title (as course_title make sure to use a good name) 2. Diffculty Level (as difficulty)   3. **Course Summary**: A detailed overview of the course (50-100 words with key as CourseSummary), including its objectives, target audience, and key learning outcomes.  4. **List of Chapters**: A minimum of 8 chapters, each with:- **Chapter Title**: A clear and descriptive title.- **Chapter Summary**: A detailed summary of the chapter’s content (at least 100 words per chapter).- **Topic List (as topics)**: A list of at least 5 specific topics(with each topic as topic) covered in the chapter, each with a brief description (2-3 sentences per topic). 5. **Additional Resources**: Suggest at least 3 resources (e.g., books, websites, or videos) relevant to the course, with a brief description of each.`
         const aiResp = await generateCourseOutline(PROMPT);
         const aiResult = JSON.parse(aiResp.response.text());
 
@@ -24,6 +25,13 @@ export async function POST(req) {
             .returning({ resp: STUDY_MATERIAL_TABLE });
 
         console.log("DB Result:", dbResult);
+        const result = await inngest.send({
+            name:'notes.generate',
+            data: {
+                course: dbResult[0].resp,
+            },
+        });
+        console.log(result);
 
         return NextResponse.json({ result: dbResult[0] });
     } catch (error) {
